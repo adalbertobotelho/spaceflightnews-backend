2a594dad1315bdcdac96d08760518d2539393cdb
Adalberto Botelho <adalbertobotelho@gmail.com>
2022-04-05 11:46:09 -0300

    fixing minor issue with prisma

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes/index.js');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 
 const swaggerDocs = require('./config/swagger.json');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(routes);
